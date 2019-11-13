a6091f4d65bb99f4542885e432c5f7364a10ad36
swallowff@163.com <yanni623>
2019-11-13 16:12:09 +0800

    add corsFilterConfig

---
+++ code/configs/cors/CorsLocalConfig.java
@@ -0,0 +1,35 @@
+package cn.huapu.power.core.cfg.weblog;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author shenyu
+ * @Description 本地跨域解决(不使用gateway的情况下需要开启)
+ * @create 2019-11-13
+ */
+@Configuration
+public class CorsLocalConfig {
+
+    /**
+     * 跨域过滤器
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+}
